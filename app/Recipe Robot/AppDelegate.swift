3dc09b6af640dc18efa9844a7e535903d99bf161
Elliot Jordan <elliot@elliotjordan.com>
2015-07-22 10:21:33 -0700

    Initial creation of stub project for Recipe Robot app in Xcode.

---
+++ app/Recipe Robot/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Recipe Robot
+//
+//  Created by Elliot Jordan on 7/19/15.
+//  Copyright (c) 2015 Elliot Jordan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
