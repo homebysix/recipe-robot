c61717442590fce633003a982273aefc436d2bc6
Eldon Ahrold <eldon.ahrold@gmail.com>
2015-10-11 23:24:18 -0500

    Colors and buttons, scrolling text view.

---
+++ app/Recipe Robot/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  Recipe Robot
+//
+//  Created by Eldon on 10/11/15.
+//  Copyright (c) 2015 Linde Group. All rights reserved.
+//
+
+import Cocoa
+
+let rrBlueColor = NSColor(SRGBRed: 88/255, green: 146/255, blue: 178/255, alpha: 1)
+let rrLtBlueColor = NSColor(SRGBRed: 152/255, green: 185/255, blue: 204/255, alpha: 1)
+let rrGreyColor = NSColor(SRGBRed: 192/255, green: 201/255, blue: 201/255, alpha: 1)
+let rrGreenColor = NSColor(SRGBRed: 135/255, green: 168/255, blue: 135/255, alpha: 1)
+let rrRedColor = NSColor(SRGBRed: 181/255, green: 33/255, blue: 38/255, alpha: 1)
+let rrYellowColor = NSColor(SRGBRed: 243/255, green: 223/255, blue: 89/255, alpha: 1)
