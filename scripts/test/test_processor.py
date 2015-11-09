dcc43f3267613738794e84504fb9d888bbfdbefe
Shea Craig <sheagcraig@gmail.com>
2015-11-08 20:23:27 -0500

    Fly the RecipeRobot to TDD land.

---
+++ scripts/test/test_processor.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# This Python file uses the following encoding: utf-8
+
+# Recipe Robot
+# Copyright 2015 Elliot Jordan, Shea G. Craig, and Eldon Ahrold
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""
+test_processor.py
+
+Unit tests for processor class.
+"""
+
+
+from nose.tools import *  # pylint: disable=unused-wildcard-import, wildcard-import
+
+from recipe_robot_lib import processor
+
+
+class TestProcessor(object):
+
+    def test_empty_to_dict(self):
+        adv = processor.AppDmgVersioner()
+        output_dict = adv.to_dict()
+        test_dict = {"Processor": "AppDmgVersioner",
+                     "Arguments": {}}
+        assert_dict_equal(output_dict, test_dict)
+
+    def test_loaded_to_dict(self):
+        adv = processor.AppDmgVersioner(dmg_path="~/Downloads/Awesome.dmg")
+        output_dict = adv.to_dict()
+        test_dict = {"Processor": "AppDmgVersioner",
+                     "Arguments": {
+                         "dmg_path": "~/Downloads/Awesome.dmg"}}
+        assert_dict_equal(output_dict, test_dict)
+
+    def test_input_variables(self):
+        adv = processor.MunkiImporter()
+        expected_variables = (
+            "MUNKI_REPO", "pkg_path", "munkiimport_pkgname",
+            "munkiimport_appname", "repo_subdirectory", "pkginfo",
+            "force_munkiimport", "additional_makepkginfo_options",
+            "version_comparison_key", "MUNKI_PKGINFO_FILE_EXTENSION")
+        assert_list_equal(sorted(adv._input_variables),
+                          sorted(expected_variables))
