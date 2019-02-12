4957585468a7c2dfeb0ff98563507c61da516358
Elliot Jordan <elliot@elliotjordan.com>
2019-02-12 15:18:22 -0800

    Version bump for development

--- scripts/recipe_robot_lib/tools.py
+++ scripts/recipe_robot_lib/tools.py
@@ -48,7 +48,7 @@ except ImportError:
     import plistlib as FoundationPlist
 
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 ENDC = "\033[0m"
 PREFS_FILE = os.path.expanduser(
     "~/Library/Preferences/com.elliotjordan.recipe-robot.plist")
