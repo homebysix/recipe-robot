ce2b96994403238b68145a3c8ddac155892d9279
Elliot Jordan <elliot@elliotjordan.com>
2019-02-25 06:38:28 -0800

    Added pkg download test

--- scripts/test/test_functional_pkg_download_url.py
+++ scripts/test/test_functional_pkg_download_url.py
@@ -40,11 +40,13 @@ from nose.tools import *
 
 
 def test():
-    # Robby is thinking of a new app to test...
-    app_name = ""
-    developer = ""
-    description = ""
-    input_path = ""
+    # Robby wants a challenge: a pkg with multiple apps inside.
+    app_name = "Kindle Previewer 3"
+    developer = "AMZN Mobile"
+    description = "Preview book layouts; ideal for publishers and authors."
+    input_path = (
+        "https://s3.amazonaws.com/kindlepreviewer3/KindlePreviewerInstaller.pkg"
+    )
 
     if not input_path:
         return
@@ -57,4 +59,70 @@ def test():
         assert_in("Input", recipes[recipe_type])
         assert_in("Process", recipes[recipe_type])
 
-    # Insert recipe-specific tests here.
+    assert_equals(
+        "https://s3.amazonaws.com/kindlepreviewer3/KindlePreviewerInstaller.pkg",
+        recipes["download"]["Input"]["DOWNLOAD_URL"],
+    )
+
+    expected_args = {"filename": "%NAME%.pkg", "url": "%DOWNLOAD_URL%"}
+    verify_processor_args("URLDownloader", recipes["download"], expected_args)
+
+    assert_in(
+        "EndOfCheckPhase",
+        [processor["Processor"] for processor in recipes["download"]["Process"]],
+    )
+
+    expected_args = {
+        "input_path": "%pathname%",
+        "expected_authority_names": [
+            "Developer ID Installer: AMZN Mobile LLC (94KV3E626L)",
+            "Developer ID Certification Authority",
+            "Apple Root CA",
+        ],
+    }
+    verify_processor_args("CodeSignatureVerifier", recipes["download"], expected_args)
+
+    expected_pkginfo = {
+        "blocking_applications": [
+            "KindlePreviewerUpdater.app",
+            "Kindle Previewer 3.app",
+        ],
+        "catalogs": ["testing"],
+        "description": description,
+        "developer": developer,
+        "display_name": app_name,
+        "name": "%NAME%",
+        "unattended_install": True,
+    }
+    assert_equals(expected_pkginfo, recipes["munki"]["Input"]["pkginfo"])
+
+    expected_args = {
+        "pkg_path": "%pathname%",
+        "repo_subdirectory": "%MUNKI_REPO_SUBDIR%",
+    }
+    verify_processor_args("MunkiImporter", recipes["munki"], expected_args)
+
+    assert_equals("Productivity", recipes["jss"]["Input"]["CATEGORY"])
+    assert_equals("%NAME%-update-smart", recipes["jss"]["Input"]["GROUP_NAME"])
+    assert_equals("SmartGroupTemplate.xml", recipes["jss"]["Input"]["GROUP_TEMPLATE"])
+    assert_equals(app_name, recipes["jss"]["Input"]["NAME"])
+    assert_equals("Testing", recipes["jss"]["Input"]["POLICY_CATEGORY"])
+    assert_equals("PolicyTemplate.xml", recipes["jss"]["Input"]["POLICY_TEMPLATE"])
+    assert_equals(description, recipes["jss"]["Input"]["SELF_SERVICE_DESCRIPTION"])
+    assert_equals("%NAME%.png", recipes["jss"]["Input"]["SELF_SERVICE_ICON"])
+
+    expected_args = {
+        "category": "%CATEGORY%",
+        "groups": [
+            {"name": "%GROUP_NAME%", "smart": True, "template_path": "%GROUP_TEMPLATE%"}
+        ],
+        "policy_category": "%POLICY_CATEGORY%",
+        "policy_template": "%POLICY_TEMPLATE%",
+        "prod_name": "%NAME%",
+        "self_service_description": "%SELF_SERVICE_DESCRIPTION%",
+        "self_service_icon": "%SELF_SERVICE_ICON%",
+    }
+    verify_processor_args("JSSImporter", recipes["jss"], expected_args)
+
+    expected_args = {"pkg_path": "%pathname%"}
+    verify_processor_args("Installer", recipes["install"], expected_args)
