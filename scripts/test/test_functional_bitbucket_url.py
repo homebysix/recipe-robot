8c26cfd7055046173e5b11626e00c793648ccd71
Elliot Jordan <elliot@elliotjordan.com>
2019-02-25 06:00:47 -0800

    Added BitBucket URL test

--- scripts/test/test_functional_bitbucket_url.py
+++ scripts/test/test_functional_bitbucket_url.py
@@ -35,23 +35,107 @@ from .test_functional import robot_runner, verify_processor_args, get_output_pat
 
 # pylint: disable=unused-wildcard-import, wildcard-import
 from nose.tools import *
-from recipe_robot_lib import FoundationPlist
 
 # pylint: enable=unused-wildcard-import, wildcard-import
 
 
 def test():
     # Robby is thinking of a new app to test...
-    return  # Remove before flight.
-    app_name = "Foo"
-    developer = "Bar Software"
-    input_path = "https://example.com/osx/latest.dmg"
+    app_name = "MDS"
+    developer = "Twocanoes Software"
+    description = "Monitor mds interference."  # Incorrect but expected.
+    input_path = "https://bitbucket.org/twocanoes/macdeploystick"
+
+    if not input_path:
+        return
 
     # Process the input and return the recipes.
     recipes = robot_runner(input_path, app_name, developer)
 
+    # Perform checks on recipes.
     for recipe_type in ("download", "pkg", "munki", "install", "jss"):
         assert_in("Input", recipes[recipe_type])
         assert_in("Process", recipes[recipe_type])
 
-    # Insert recipe-specific tests here.
+    assert_equals(
+        "https://www.twocanoes.com/macdeploystick/updates/macdeploystick-software-updates.xml",
+        recipes["download"]["Input"]["SPARKLE_FEED_URL"],
+    )
+
+    expected_args = {"appcast_url": "%SPARKLE_FEED_URL%"}
+    verify_processor_args(
+        "SparkleUpdateInfoProvider", recipes["download"], expected_args
+    )
+
+    expected_args = {"filename": "%NAME%-%version%.dmg"}
+    verify_processor_args("URLDownloader", recipes["download"], expected_args)
+
+    assert_in(
+        "EndOfCheckPhase",
+        [processor["Processor"] for processor in recipes["download"]["Process"]],
+    )
+
+    expected_args = {
+        "input_path": "%pathname%/MDS.app",
+        "requirement": (
+            'anchor apple generic and identifier "com.twocanoes.MacDeployStick" and '
+            "(certificate leaf[field.1.2.840.113635.100.6.1.9] /* exists */ or "
+            "certificate 1[field.1.2.840.113635.100.6.2.6] /* exists */ and "
+            "certificate leaf[field.1.2.840.113635.100.6.1.13] /* exists */ and "
+            "certificate leaf[subject.OU] = UXP6YEHSPW)"
+        ),
+    }
+    verify_processor_args("CodeSignatureVerifier", recipes["download"], expected_args)
+
+    assert_equals("com.twocanoes.MacDeployStick", recipes["pkg"]["Input"]["BUNDLE_ID"])
+
+    assert_in(
+        "AppPkgCreator",
+        [processor["Processor"] for processor in recipes["pkg"]["Process"]],
+    )
+
+    expected_pkginfo = {
+        "catalogs": ["testing"],
+        "description": description,
+        "developer": developer,
+        "display_name": app_name,
+        "name": "%NAME%",
+        "unattended_install": True,
+    }
+    assert_equals(expected_pkginfo, recipes["munki"]["Input"]["pkginfo"])
+
+    expected_args = {
+        "pkg_path": "%pathname%",
+        "repo_subdirectory": "%MUNKI_REPO_SUBDIR%",
+    }
+    verify_processor_args("MunkiImporter", recipes["munki"], expected_args)
+
+    assert_equals("Productivity", recipes["jss"]["Input"]["CATEGORY"])
+    assert_equals("%NAME%-update-smart", recipes["jss"]["Input"]["GROUP_NAME"])
+    assert_equals("SmartGroupTemplate.xml", recipes["jss"]["Input"]["GROUP_TEMPLATE"])
+    assert_equals(app_name, recipes["jss"]["Input"]["NAME"])
+    assert_equals("Testing", recipes["jss"]["Input"]["POLICY_CATEGORY"])
+    assert_equals("PolicyTemplate.xml", recipes["jss"]["Input"]["POLICY_TEMPLATE"])
+    assert_equals(description, recipes["jss"]["Input"]["SELF_SERVICE_DESCRIPTION"])
+    assert_equals("%NAME%.png", recipes["jss"]["Input"]["SELF_SERVICE_ICON"])
+
+    expected_args = {
+        "category": "%CATEGORY%",
+        "groups": [
+            {"name": "%GROUP_NAME%", "smart": True, "template_path": "%GROUP_TEMPLATE%"}
+        ],
+        "policy_category": "%POLICY_CATEGORY%",
+        "policy_template": "%POLICY_TEMPLATE%",
+        "prod_name": "%NAME%",
+        "self_service_description": "%SELF_SERVICE_DESCRIPTION%",
+        "self_service_icon": "%SELF_SERVICE_ICON%",
+    }
+    verify_processor_args("JSSImporter", recipes["jss"], expected_args)
+
+    expected_args = {
+        "dmg_path": "%pathname%",
+        "items_to_copy": [
+            {"destination_path": "/Applications", "source_item": "MDS.app"}
+        ],
+    }
+    verify_processor_args("InstallFromDMG", recipes["install"], expected_args)
