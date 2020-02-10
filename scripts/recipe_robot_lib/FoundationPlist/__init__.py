3b367d68104f9be251d023190609b83bf6d4490f
Elliot Jordan <elliot@elliotjordan.com>
2020-02-10 14:43:47 -0800

    Revert "Remove FoundationPlist from project"
    This reverts commit ddc2036e1f24701a5cab3606188f27e0ce3b7fa7.

---
+++ scripts/recipe_robot_lib/FoundationPlist/__init__.py
@@ -0,0 +1,11 @@
+# We're using this package as a module.
+from __future__ import absolute_import, print_function
+
+try:
+    from .FoundationPlist import *
+except ImportError:
+    print(
+        "WARNING: using 'from plistlib import *' instead of "
+        "'from FoundationPlist import *' in " + __name__
+    )
+    from plistlib import *
